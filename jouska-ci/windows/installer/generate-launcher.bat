2250e1c2b79a07e576f9fa247b05a7c06f7c62ff
Tigran Sargsyan <tigran.sargsyan@synisys.com>
2020-02-04 20:59:10 +0400

    Add bat-to-exe-converter.

--- jouska-ci/windows/installer/generate-launcher.bat
+++ jouska-ci/windows/installer/generate-launcher.bat
@@ -1,1 +1,1 @@
- "C:\Program Files\Bat To Exe Converter\Bat_To_Exe_Converter.exe" /bat %1 /exe %2 /icon %3 /invisible
+"jouska-ci\windows\installer\bat-to-exe-converter.exe" /bat %1 /exe %2 /icon %3 /invisible
