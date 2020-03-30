6d8ff8dcdb3b713f3b5968b91e6b30335616dce1
Tigran Sargsyan <tigran.sargsyan@synisys.com>
2020-03-30 14:14:50 +0400

    Fix linux build.

--- jouska-ci/unix/linux/linux-build.sh
+++ jouska-ci/unix/linux/linux-build.sh
@@ -2,5 +2,5 @@
 # $2 Linux Javafx Jmods path
 
 sh jouska-ci/unix/unix-build.sh LINUX "$1" "$2" && \
-cp -R jouska-ci/unix/linux/include/ jouska-client/target/ && \
+cp -R jouska-ci/unix/linux/include/* jouska-client/target/output && \
 sh jouska-ci/unix/installer/generate-installer.sh jouska-client/target/output/jouska-linux-setup.run
