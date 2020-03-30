795dca7f81060f5b0854ec6383976eb2fad89f96
Tigran Sargsyan <tigran.sargsyan@synisys.com>
2020-03-30 23:22:27 +0400

    Add output folder in build scripts.

--- jouska-ci/build/unix/linux/linux-build.sh
+++ jouska-ci/build/unix/linux/linux-build.sh
@@ -1,7 +1,9 @@
 # $1 maven post command (now used for properties) (example -Djouska.server.host=1.1.1.1 -Djouska.server.port=8888)
 # $2 Linux JDK path
 # $3 Linux Javafx Jmods path
+# $4 Output folder
 
-sh jouska-ci/build/unix/unix-build.sh LINUX "$1" "$2" "$3" && \
+sh jouska-ci/build/base/build.sh "$1 -Dos=LINUX" "$2" "$3" && \
+cp -R jouska-ci/build/unix/installer/include/* jouska-client/target/output && \
 cp -R jouska-ci/build/unix/linux/include/* jouska-client/target/output && \
-sh jouska-ci/build/unix/installer/generate-installer.sh jouska-client/target/output jouska-client/target/output/jouska-linux-setup.sh
+sh jouska-ci/build/unix/installer/generate-installer.sh jouska-client/target/output "$4"/jouska-linux-setup.sh
--- jouska-ci/build/unix/unix-build.sh
+++
@@ -1,8 +0,0 @@
-# $1 values [LINUX,MAC]
-# $2 maven post command (now used for properties) (example -Dos=LINUX -Djouska.server.host=1.1.1.1 -Djouska.server.port=8888)
-# $3 JDK path (platform should be according to the first parameter)
-# $4 Javafx Jmods path (platform should be according to the first parameter)
-
-sh jouska-ci/build/base/build.sh "$2 -Dos=$1" "$3" "$4" && \
-cp -R jouska-ci/build/unix/installer/include/* jouska-client/target/output
-
