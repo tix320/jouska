3d85d9a630d81c788ccee02cbf18aeb6e8182282
Tigran Sargsyan <tigran.sargsyan@synisys.com>
2020-03-30 10:56:07 +0400

    Fix build script.

--- jouska-ci/scripts/build.sh
+++ jouska-ci/scripts/build.sh
@@ -2,7 +2,7 @@
 # $2 JDK path (platform should be according to the first parameter)
 # $3 Javafx Jmods path (platform should be according to the first parameter)
 
-cd mvn clean install -Dos="$1" \
+mvn clean install -Dos="$1" \
 sh jouska-ci/target/appassembler/bin/jre.sh jouska-client/target "$2" "$3" && \
 cp "jouska-client/target/classes/config.properties" "jouska-client/target/config.properties" && \
 rmdir -r jouska-client/target/classes jouska-client/target/generated-sources jouska-client/target/generated-test-sources jouska-client/target/test-classes \
