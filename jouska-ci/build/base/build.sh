f1112bbcda35920b7652a494489d2f60f2229f0e
Tigran Sargsyan <tigran.sargsyan@synisys.com>
2020-03-31 00:16:36 +0400

    Add missing client jar to client build.

--- jouska-ci/build/base/build.sh
+++ jouska-ci/build/base/build.sh
@@ -5,4 +5,5 @@
 mvn clean install -B "$1" && \
 mkdir jouska-client/target/output && \
 sh jouska-ci/target/appassembler/bin/jre.sh unix jouska-client/target/lib "$2" "$3" jouska-client/target/output && \
-cp "jouska-client/target/classes/config.properties" "jouska-client/target/output/config.properties"
+cp jouska-client/target/classes/config.properties jouska-client/target/output/config.properties && \
+cp jouska-client/target/classes/jouska-app.jar jouska-client/target/output/jouska-app.jar
