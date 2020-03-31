f955dc49f78d1dee2b2330ce2f4c379849bb7c6e
Tigran Sargsyan <tigran.sargsyan@synisys.com>
2020-03-31 15:55:36 +0400

    Fix bot module name in runner script.

--- jouska-ci/build/unix/static/jouska-bot.sh
+++ jouska-ci/build/unix/static/jouska-bot.sh
@@ -1,1 +1,1 @@
-chmod +x jre/bin/java && jre/bin/java  --module-path jouska-bot.jar -m jouska.client/com.github.tix320.jouska.bot.BotApp
+chmod +x jre/bin/java && jre/bin/java  --module-path jouska-bot.jar -m jouska.bot/com.github.tix320.jouska.bot.BotApp
