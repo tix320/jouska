de910a965b66e5e6faeb04a53b18d0da7604bc36
Tigran Sargsyan <tigran.sargsyan@synisys.com>
2020-05-02 00:00:30 +0400

    Fix server config file read.

--- jouska-server/src/main/java/com/github/tix320/jouska/server/app/Configuration.java
+++ jouska-server/src/main/java/com/github/tix320/jouska/server/app/Configuration.java
@@ -1,6 +1,5 @@
 package com.github.tix320.jouska.server.app;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Path;
@@ -13,7 +12,7 @@ public class Configuration {
 	private static final Map<String, String> properties = new HashMap<>();
 
 	static {
-		try (InputStream inputStream = new FileInputStream("config.properties")) {
+		try (InputStream inputStream = Configuration.class.getResourceAsStream("/config.properties")) {
 			Properties properties = new Properties();
 			properties.load(inputStream);
 			Configuration.properties.putAll((Map) properties);
