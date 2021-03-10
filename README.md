159e9ad5a92b33b3cb52a04a0adbc5c3e5cf2ac1
Tigran Sargsyan <tixtix320@gmail.com>
2021-03-10 22:00:18 +0400

    Create images via maven plugin.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # jouska
-## The java/javafx version of game Clonium.
-![Build](https://travis-ci.com/tix320/jouska.svg?branch=master "Build pass")
+
+**The java/javafx version of game 'Clonium'.**
+
--- jouska-server/src/main/java/com/github/tix320/jouska/server/app/Configuration.java
+++ jouska-server/src/main/java/com/github/tix320/jouska/server/app/Configuration.java
@@ -5,8 +5,8 @@ import java.net.InetSocketAddress;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-import com.github.tix320.jouska.core.util.JouskaProperties;
 import com.github.tix320.deft.api.SystemProperties;
+import com.github.tix320.jouska.core.util.JouskaProperties;
 
 public class Configuration {
 
@@ -17,12 +17,13 @@ public class Configuration {
 	public Configuration() {
 		int port;
 		String portString = SystemProperties.getFromEnvOrElseJava(JouskaProperties.SERVER_PORT);
-		port = portString == null ? 8888 : Integer.parseInt(portString);
+		port = portString == null ? JouskaProperties.DEFAULT_SERVER_PORT : Integer.parseInt(portString);
 		InetSocketAddress serverAddress;
 		try {
 			serverAddress = new InetSocketAddress(port);
-		} catch (IllegalArgumentException e) {
-			serverAddress = new InetSocketAddress("localhost", 8888);
+		} catch (IllegalArgumentException ignored) {
+			serverAddress = new InetSocketAddress(JouskaProperties.DEFAULT_SERVER_HOST,
+					JouskaProperties.DEFAULT_SERVER_PORT);
 		}
 
 		String clientAppPath = SystemProperties.getFromEnvOrElseJava(JouskaProperties.CLIENT_APP_PATH, "client-app");
